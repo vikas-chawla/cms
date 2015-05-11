30ed366f31ddd5077aace56a12114585f057d2e6
Vikas Chawla <vikas.st.chawla@gmail.com>
2015-05-10 23:38:46 -0500

    refactored index.php

---
+++ includes/navigation.php
@@ -0,0 +1,50 @@
+ <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="./index.php">CMS</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+
+                <?php
+                $query = "SELECT * FROM categories";
+                $select_all = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_assoc($select_all) )
+                {
+                    $category_title = $row['category_title'];
+
+                    echo "<li><a href='#'>{$category_title}</a></li>";
+                }
+
+
+
+                ?>         
+
+
+
+                    <li>
+                        <a href="Admin">Admin</a>
+                    </li>
+                    <!--
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                    -->
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
