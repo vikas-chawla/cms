f8347c07c59adf8a2fe036404a5539cda1fa77fe
Vikas Chawla <vikas.st.chawla@gmail.com>
2015-05-11 00:16:22 -0500

    Refactored and created functions page

---
+++ admin/functions.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+function confirmQuery($result)
+{
+    
+    if(!$create_post_query )
+    {
+        die("QUERY FAILED ." . mysqli_error($connection) );
+    }
+
+
+}
+*/
+
+function insert_categories(&$connection)
+{
+    if(isset($_POST['submit']) )
+    {
+    	//global $connection;
+        $new_category_title = $_POST['category_title'];
+
+        if($new_category_title == "" || empty($new_category_title) )  
+        {
+            echo "The field below should not be empty";
+        }     
+        $query = $connection->prepare("INSERT INTO categories SET category_title = ?;");
+        $query->bind_param("s", $new_category_title);
+        $success = $query->execute();
+        
+        /*$query = "INSERT INTO categories(category_title) ";
+        $query .= "VALUE('{$new_category_title}') ";
+        $create_new_category_query = mysqli_query($connection, $query);
+        */
+    	
+        if(!$success)
+    	{
+        	die('QUERY FAILED' . mysqli_error($connection) );
+    	}
+    }
+}
+
+
+
+function findAllCategories(&$connection)
+{
+
+	$query = "SELECT * FROM categories";
+    $select_categories = mysqli_query($connection, $query);
+    
+    while($row = mysqli_fetch_assoc($select_categories) )
+    {
+        $category_id = $row['category_id'];
+        $category_title = $row['category_title'];
+       
+        echo "<tr>";
+        echo "<td>{$category_id}</td>";
+        echo "<td>{$category_title}</td>";
+        echo "<td><a href='categories.php?delete={$category_id}'>Delete</a></td>";
+        echo "<td><a href='categories.php?edit={$category_id}'>Edit</a></td>";
+        echo "</tr>";
+    }
+}
+
+function deleteCategories(&$connection)
+{
+	if( isset($_GET['delete']) )
+	{
+	    $delete_category_id = $_GET['delete'];
+	    $query = "DELETE FROM categories WHERE category_id = {$delete_category_id} ";
+	    $delete_query = mysqli_query($connection,$query);
+	    header("Location: categories.php");
+	}
+}
+
+
+?>
