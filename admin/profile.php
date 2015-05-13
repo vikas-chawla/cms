5eaed98ff04c4a09137f691babaab1a2bd2a86b0
Vikas Chawla <vikas.st.chawla@gmail.com>
2015-05-12 23:00:26 -0500

    added a profile page with form that gets data based off sessions from login

--- admin/profile.php
+++ admin/profile.php
@@ -2,14 +2,82 @@
 <?php
     if(isset($_SESSION['user_username']) )
     {
-        echo $_SESSION['user_username'];
+        $username =  $_SESSION['user_username'];
+        $query = "SELECT * FROM users WHERE username = '{$username}'";
+        $select_user_profile_query = mysqli_query($connection, $query);
+        while($row = mysqli_fetch_array($select_user_profile_query) )
+        {
+            $user_id = $row['user_id'];  
+            $username = $row['username'];  
+            $user_password = $row['user_password'];  
+            $user_firstname = $row['user_firstname'];  
+            $user_lastname = $row['user_lastname'];  
+            $user_email = $row['user_email'];  
+            $user_image = $row['user_image'];  
+            $user_role = $row['user_role'];  
+        }
     }
 
 
 ?>
-<body>
-    <div id="wrapper">
 
+    <?php
+    if(isset($_POST['edit_profile']) )
+    {
+    $query = "UPDATE users SET ";
+    $query .="user_firstname = '{$user_firstname}', ";
+    $query .="user_lastname = '{$user_lastname}', ";
+    $query .="user_role = '{$user_role}', ";
+    $query .="username = '{$username}', ";
+    $query .="user_email = '{$user_email}', ";
+    $query .="user_password = '{$user_password}' ";
+
+    $query .="WHERE username = {$username} ";
+
+    $update_user = mysqli_query($connection,$query);
+
+    if(!$update_user)
+    {
+        die("QUERY FAILED" . mysqli_error($connection) );
+    }
+
+    $user_firstname = $_POST['user_firstname'];
+    $user_lastname = $_POST['user_lastname'];
+    $user_role = $_POST['user_role'];
+    
+    /*
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];*/
+    
+    $username = $_POST['username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+    //$post_date = date('d-m-y');
+    //$post_comment_count = 0;
+
+    //move_uploaded_file($post_image_temp, "../Images/$post_image");
+
+    $the_user_id = $_GET['edit_profile'];
+    $query = "UPDATE users SET ";
+    $query .="user_firstname = '{$user_firstname}', ";
+    $query .="user_lastname ='{$user_lastname}', ";
+    $query .="user_role ='{$user_role}', ";
+    $query .="username = '{$username}', ";
+    $query .="user_email = '{$user_email}', ";
+    $query .="user_password = '{$user_password}' ";  
+    $query .="WHERE user_id = {$the_user_id} ";
+    $update_user = mysqli_query($connection,$query);
+
+    if(!$update_user)
+    {
+        die($query . "QUERY FAILED" . mysqli_error($connection) );
+    }
+        
+}
+
+    ?>
+    <div id="wrapper">
+<body>
         <!-- Navigation -->
         <?php include "includes/admin_navigation.php" ?>
 
@@ -40,18 +108,17 @@
 
     <div class="form-group">
     <select name="user_role" id="">
+        <option value="subscriber"><?php echo $user_role; ?></option>
         <?php
         if($user_role == 'admin')
         {
-            echo "<option value='subscriber'>Subscriber></option>";
+            echo "<option value='subscriber'>Subscriber</option>";
         }
         else
         {
             echo "<option value='admin'>Admin</option>";            
         }
         ?>
-
-        <option value="subscriber">Select Options</option>
         <option value="admin">Admin</option>
     </select>
     </div>
@@ -79,7 +146,7 @@
 
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="edit_user" value="Update User">
+        <input class="btn btn-primary" type="submit" name="edit_profile" value="Update Profile">
     </div>
 </form> 
 
