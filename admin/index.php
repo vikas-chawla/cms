58fd4d1a999039d45c57bf7412561d0ae70d60cd
Vikas Chawla <vikas.st.chawla@gmail.com>
2015-05-14 22:38:22 -0500

    dynamically represented users and categories

--- admin/index.php
+++ admin/index.php
@@ -92,7 +92,13 @@
                         <i class="fa fa-user fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
-                    <div class='huge'>23</div>
+                      <?php
+                        $query = "SELECT * FROM users";
+                        $select_all_users = mysqli_query($connection, $query);
+                        $post_users = mysqli_num_rows($select_all_users);
+                        
+                        echo "<div class='huge'>{$post_users}</div>"
+                        ?>
                         <div> Users</div>
                     </div>
                 </div>
@@ -114,8 +120,14 @@
                         <i class="fa fa-list fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
-                        <div class='huge'>13</div>
-                         <div>Categories</div>
+                          <?php
+                        $query = "SELECT * FROM categories";
+                        $select_all_categories = mysqli_query($connection, $query);
+                        $post_categories = mysqli_num_rows($select_all_categories);
+                        
+                        echo "<div class='huge'>{$post_categories}</div>"
+                        ?>
+                        <div>Categories</div>
                     </div>
                 </div>
             </div>
